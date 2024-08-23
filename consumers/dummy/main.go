b563931e5c293eebb28626396f401504f4a980da
shreynadiadwala <shrey.nadiadwala@lyearn.com>
2024-08-23 16:01:53 +0530

    feat: consumers

---
+++ consumers/dummy/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"time"
+
+	"github.com/shreynadiadwala/Person-Lyearn-Testing-Backend-Universe/packages/timeutil"
+)
+
+func GetCurrentTime() time.Time {
+	return timeutil.GetCurrentTime()
+}
