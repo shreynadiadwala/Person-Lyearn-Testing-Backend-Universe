ba03635a8cb6600e91cb6618f6270d736b5a2664
shreynadiadwala <shrey.nadiadwala@lyearn.com>
2024-08-23 13:31:57 +0530

    Update calculator service to use mathutil v0.1.0

---
+++ services/calculator/go.mod
@@ -0,0 +1,5 @@
+module github.com/yourusername/Person-Lyearn-Testing-Backend-Universe/services/calculator
+
+go 1.22.4
+
+require github.com/shreynadiadwala/Person-Lyearn-Testing-Backend-Universe/packages/mathutil v0.0.0-20240823075021-1efb05343c12
---
+++ services/calculator/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shreynadiadwala/Person-Lyearn-Testing-Backend-Universe/packages/mathutil"
+)
+
+func main() {
+	fmt.Println("Add: ", mathutil.Add(2, 3))
+	fmt.Println("Multiply: ", mathutil.Multiply(2, 3))
+}
